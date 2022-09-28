eb593549cd0ccf1685fc6b40b07723ed0a854b7c
RhysSully <rhys.osullivan@yahoo.co.uk>
2022-09-28 12:39:48 +0100

    Form inputs, post and get HTTP requests

--- react-course-p2/src/pages/NewMeetup.js
+++ react-course-p2/src/pages/NewMeetup.js
@@ -1,5 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>;
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-course-p2-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/', 'replace');
+        });
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
+    );
 }
 
 export default NewMeetupPage;
